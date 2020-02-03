9d15757f42d3db6138e33b94eac2108284810a93
Ferza17 <feryrza85@gmail.com>
2020-02-03 08:53:19 +0700

    update GridView.builder()

---
+++ lib/widgets/products_item.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem(this.id, this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+        ),
+      ),
+      footer: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTileBar(
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
+          backgroundColor: Colors.black87,
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
